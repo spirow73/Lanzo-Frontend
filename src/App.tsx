dd7164d99cc3fb647647b1dc002ae21a9bb0f6b0
spirow73 <67395826+spirow73@users.noreply.github.com>
2025-02-26 19:31:11 +0100

    Created shadcn components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
+import { Button } from "@/components/ui/button";
 import './App.css';
 
 function App() {
@@ -14,9 +15,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <Button onClick={() => setCount((count) => count + 1)}>count is {count}</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
